71efba165fa45d630c87ba01bfd45ae0f9e1181e
Justin Simonelli <justin.simonelli@sherwin.com>
2013-12-28 19:13:24 -0500

    Adding timestamp column back

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 class Todos(db.Model):
 	__tablename__ = 'TODOS'
 	id = db.Column(db.INTEGER, primary_key = True, autoincrement = True)
+	timestamp = db.Column(db.TIMESTAMP)
 	message = db.Column(db.VARCHAR(4000))
 	latitude = db.Column(db.VARCHAR(20))
 	longitude = db.Column(db.VARCHAR(20))
